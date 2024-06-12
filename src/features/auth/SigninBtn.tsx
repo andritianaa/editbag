75374b1a86e642608e092018e4d5ef10888c81a7
Rakotonimanana T. Andritiana Steve <andritianasteve@gmail.com>
2024-06-13 00:33:32 +0300

    add

--- src/features/auth/SigninBtn.tsx
+++ src/features/auth/SigninBtn.tsx
@@ -3,19 +3,26 @@
 import { Button } from "@/components/ui/button";
 import { LogIn } from "lucide-react";
 import { SignInAction } from "@/actions/auth.actions";
+import Link from "next/link";
 
 export const SigninBtn = () => {
   return (
-    <form>
-      <button
-        onClick={async (e) => {
-          e.preventDefault();
-          await SignInAction();
-        }}
-        className="pinkButton justify-centerh-12 relative flex h-10 items-center overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-      >
+    // <form>
+    //   <button
+    //     onClick={async (e) => {
+    //       e.preventDefault();
+    //       await SignInAction();
+    //     }}
+    //     className="pinkButton justify-centerh-12 relative flex h-10 items-center overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+    //   >
+    //     <p className="px-6">Sign in</p>
+    //   </button>
+    // </form>
+
+    <Link href="/signin">
+      <button className="pinkButton justify-centerh-12 relative flex h-10 items-center overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
         <p className="px-6">Sign in</p>
       </button>
-    </form>
+    </Link>
   );
 };
