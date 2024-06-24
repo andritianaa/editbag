8ea4860a22ee69e8bddc707a8dd9b22331c7cae2
Rakotonimanana T. Andritiana Steve <andritianasteve@gmail.com>
2024-06-24 22:42:59 +0300

    color active tab

--- src/components/products/Tab.tsx
+++ src/components/products/Tab.tsx
@@ -1,74 +1,72 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { BackgroundGradient } from "@/components/ui/background-gradient";
 
 export type TabProps = {
   type: string;
 };
 
 export const Tab = (props: TabProps) => {
+  const links = [
+    {
+      href: "/products",
+      type: "templates",
+      imgSrc: "/illustration/template.png",
+      alt: "template",
+      text: "Templates",
+    },
+    {
+      href: "/products/emoji",
+      type: "emoji",
+      imgSrc: "/illustration/emoji.png",
+      alt: "emoji",
+      text: "Emoji",
+    },
+    {
+      href: "/products/images",
+      type: "images",
+      imgSrc: "/illustration/images.png",
+      alt: "images",
+      text: "Images",
+    },
+    {
+      href: "/products/videos",
+      type: "videos",
+      imgSrc: "/illustration/video.png",
+      alt: "video",
+      text: "Videos",
+    },
+  ];
   return (
     <div className="mt-6 flex gap-4">
-      <Link className="flex" href="/products">
-        <Button
-          variant="ghost"
-          className={`w-36 h-32 p-4 flex-col justify-between hover:bg-[rgba(38,38,38,.9)] ${
-            props.type == "templates" ? "bg-[rgba(38,38,38,.9)]" : ""
-          } `}
-        >
-          <img
-            className="size-[3.75rem]"
-            src="/illustration/template.png"
-            alt="template"
-          />
-          <p className="text-lg">Templates</p>
-        </Button>
-      </Link>
-      <Link className="flex" href="/products/emoji">
-        <Button
-          variant="ghost"
-          className={`w-36 h-32 p-4 flex-col justify-between hover:bg-[rgba(38,38,38,.9)] ${
-            props.type == "emoji" ? "bg-[rgba(38,38,38,.9)]" : ""
-          } `}
-        >
-          <img
-            className="size-[3.75rem]"
-            src="/illustration/emoji.png"
-            alt="emoji"
-          />
-          <p className="text-lg">Emoji</p>
-        </Button>
-      </Link>
-      <Link className="flex" href="/products/images">
-        <Button
-          variant="ghost"
-          className={`w-36 h-32 p-4 flex-col justify-between hover:bg-[rgba(38,38,38,.9)] ${
-            props.type == "images" ? "bg-[rgba(38,38,38,.9)]" : ""
-          } `}
-        >
-          <img
-            className="size-[3.75rem]"
-            src="/illustration/images.png"
-            alt="images"
-          />
-          <p className="text-lg">Images</p>
-        </Button>
-      </Link>
-      <Link className="flex" href="/products/videos">
-        <Button
-          variant="ghost"
-          className={`w-36 h-32 p-4 flex-col justify-between hover:bg-[rgba(38,38,38,.9)] ${
-            props.type == "videos" ? "bg-[rgba(38,38,38,.9)]" : ""
-          } `}
-        >
-          <img
-            className="size-[3.75rem]"
-            src="/illustration/video.png"
-            alt="video"
-          />
-          <p className="text-lg">Videos</p>
-        </Button>
-      </Link>
+      {links.map((link) => {
+        const content = (
+          <Link key={link.type} className="flex" href={link.href}>
+            <Button
+              variant="ghost"
+              className={`w-36 h-32 p-4 flex-col justify-between hover:bg-[rgba(38,38,38,.9)] ${
+                props.type === link.type ? "bg-[rgba(38,38,38,.9)]" : ""
+              }`}
+            >
+              <img
+                className="size-[3.75rem]"
+                src={link.imgSrc}
+                alt={link.alt}
+              />
+              <p className="text-lg">{link.text}</p>
+            </Button>
+          </Link>
+        );
+
+        return props.type === link.type ? (
+          <BackgroundGradient key={link.type} className="rounded-xl">
+            {content}
+          </BackgroundGradient>
+        ) : (
+          content
+        );
+      })}
     </div>
   );
 };
