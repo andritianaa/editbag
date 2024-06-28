b824f89d7f7eb1803a13b4f930fafa9afc5a16a8
Rakotonimanana T. Andritiana Steve <andritianasteve@gmail.com>
2024-06-28 21:31:07 +0300

    tab active color

--- src/components/ui/background-gradient.tsx
+++ src/components/ui/background-gradient.tsx
@@ -21,48 +21,31 @@ export const BackgroundGradient = ({
       backgroundPosition: ["0, 50%", "100% 50%", "0 50%"],
     },
   };
+
   return (
     <div className={cn("relative p-[4px] group", containerClassName)}>
       <motion.div
         variants={animate ? variants : undefined}
         initial={animate ? "initial" : undefined}
         animate={animate ? "animate" : undefined}
-        transition={
-          animate
-            ? {
-                duration: 5,
-                repeat: Infinity,
-                repeatType: "reverse",
-              }
-            : undefined
-        }
         style={{
-          backgroundSize: animate ? "400% 400%" : undefined,
+          backgroundSize: "400% 400%",
         }}
         className={cn(
-          "absolute inset-0 rounded-lg z-[1] opacity-60 group-hover:opacity-100 blur-xl  transition duration-500 will-change-transform",
-          " bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
+          "absolute inset-0 rounded-lg z-[1] opacity-60 group-hover:opacity-100 blur-xl ",
+          "bg-[radial-gradient(circle_farthest-side_at_0_100%,#1da2ff,transparent),radial-gradient(circle_farthest-side_at_0_0,#6effa7,#1da2ff)]"
         )}
       />
       <motion.div
         variants={animate ? variants : undefined}
         initial={animate ? "initial" : undefined}
         animate={animate ? "animate" : undefined}
-        transition={
-          animate
-            ? {
-                duration: 5,
-                repeat: Infinity,
-                repeatType: "reverse",
-              }
-            : undefined
-        }
         style={{
-          backgroundSize: animate ? "400% 400%" : undefined,
+          backgroundSize: "400% 400%",
         }}
         className={cn(
           "absolute inset-0 rounded-lg z-[1] will-change-transform",
-          "bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
+          "bg-[radial-gradient(circle_farthest-side_at_0_100%,#1da2ff,transparent),radial-gradient(circle_farthest-side_at_0_0,#6effa7,#1da2ff)]"
         )}
       />
 
